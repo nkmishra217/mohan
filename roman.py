9277999d1624eb08cfb5d4af55cbb7f4d2ad1b8a
Mohanakrishnan1199 <51222571+Mohanakrishnan1199@users.noreply.github.com>
2019-07-12 16:37:32 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,6 @@
+rom=input()
+n={'I':1,'II':2,'III':3,'IV':4,'V':5,'VI':6,'VII':7,'VIII':8,'IX':9,'X':10,'XI':11,'XII':12,'XIII':13,'XIV':14,'XV':15,'XVI':16,'XVII':17,'XIX':19,'XX':20}
+print(n[rom])
+
+
+
