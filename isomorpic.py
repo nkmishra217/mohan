e82abb0f78a06af4dc55ee0b09c64645540b1115
Mohanakrishnan1199 <51222571+Mohanakrishnan1199@users.noreply.github.com>
2019-07-12 17:21:22 +0530

    Create isomorpic.py

---
+++ isomorpic.py
@@ -0,0 +1,10 @@
+m,h=map(str,input().split())
+if(len(m)!=len(h)):
+    print("no")
+else :
+    s1=[m.count(i) for i in m]
+    s2=[h.count(i) for i in h]
+if(s1==s2):
+    print("yes")
+else:
+    print("no")
