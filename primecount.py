4471aa090c1e3e1367b2fa3e13393e34dffdc576
Mohanakrishnan1199 <51222571+Mohanakrishnan1199@users.noreply.github.com>
2019-07-12 18:16:32 +0530

    Create primecount.py

---
+++ primecount.py
@@ -0,0 +1,9 @@
+v1,v2=map(int,input().split())
+v3=[]
+for k in range(v1,v2+1):
+    for p in range(2,k):
+        if(k%p==0):
+                break
+    else:
+        v3.append(k)
+print(len(v3))
