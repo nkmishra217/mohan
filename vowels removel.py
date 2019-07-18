e15d1deda3f92847ac28a7047ff270a8e1246bd5
Mohanakrishnan1199 <51222571+Mohanakrishnan1199@users.noreply.github.com>
2019-07-18 16:31:26 +0530

    Create vowels removel.py

---
+++ vowels removel.py
@@ -0,0 +1,11 @@
+mo=eval(input())
+moh=(input())
+count=""
+for i in moh:
+    if i in ('A','E','I','O','U','a','e','i','o','u'):
+        pass
+    else:
+        count=count+i
+        
+        
+print(count)     
