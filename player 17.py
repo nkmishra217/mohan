36cd7bbec3668f65c78dd4424125882f8f68dad0
Mohanakrishnan1199 <51222571+Mohanakrishnan1199@users.noreply.github.com>
2019-07-22 14:11:53 +0530

    Update player 17.py

--- player 17.py
+++ player 17.py
@@ -3,12 +3,10 @@ if n>m:
     maxi=n
 else:
     maxi=m
-i=1
 while(True):
     if (maxi%n==0)and(maxi%m==0):
         low=maxi
         break
-    i=i+1
-    maxi=maxi*i
+    maxi=maxi+1
 print(low)
         
