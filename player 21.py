20afb68ae16f8c688291a2b471d607dc305a0600
Mohanakrishnan1199 <51222571+Mohanakrishnan1199@users.noreply.github.com>
2019-07-23 15:33:57 +0530

    Update player 21.py

--- player 21.py
+++ player 21.py
@@ -3,6 +3,8 @@ k,r=map(int,input().split())
 o,a=map(int,input().split())
 if (((m==k) and(k==o))or((n==r)and(r==a))):
   print("yes")
+elif((m==n) and(k==r)and (o==a)): 
+  print("yes")
 else:
   print("no")
 
